7e8377aa3a9c5ea561d0fdd42175dd1ea67db33c
postfull <saeful.mahmud@gmail.com>
2020-01-03 20:19:09 +0700

    Exercise 3.1 : Add Name In Model MembershipType -> add-migration adjust to changes -> update-database -> add-migration setNameMembership -> update-database

---
+++ Cwmosh/Migrations/202001031310347_AddNameInMembershipType.cs
@@ -0,0 +1,18 @@
+namespace Cwmosh.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameInMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "Name", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.MembershipTypes", "Name");
+        }
+    }
+}
