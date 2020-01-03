22b025e6a86968c18b558a0de82c3162a3e56d4c
postfull <saeful.mahmud@gmail.com>
2020-01-03 09:32:32 +0700

    Action Parameters

--- Cwmosh/Controllers/MoviesController.cs
+++ Cwmosh/Controllers/MoviesController.cs
@@ -13,11 +13,21 @@ namespace Cwmosh.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Froozzeen!!" };
-            // return View(movie);
-            //return Content("Hello World...!!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            return RedirectToAction("Index", "Home",new { page = 1, sortBy = "name"});
+             return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
