2b1a1f14ba4ee19ef83972dd9e7bc50f57607c11
postfull <saeful.mahmud@gmail.com>
2020-01-07 18:08:18 +0700

    Validation Summary

--- Cwmosh/Controllers/CustomersController.cs
+++ Cwmosh/Controllers/CustomersController.cs
@@ -28,6 +28,7 @@ namespace Cwmosh.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipType = membershipTypes
             };
             return View("CustomerForm",viewModel);
